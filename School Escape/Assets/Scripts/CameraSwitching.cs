ab42a23159335e93d13af020038d125f982fb6c3
Doggitoz <cpwheeler4@gmail.com>
2023-03-05 15:17:54 -0600

    Changed Camera Script

--- School Escape/Assets/Scripts/CameraSwitching.cs
+++ School Escape/Assets/Scripts/CameraSwitching.cs
@@ -6,7 +6,16 @@ public class CameraSwitching : MonoBehaviour
 {
     int index = 0;
     public GameObject[] cameras;
-    
+
+
+    private void Start()
+    {
+        for (int i = 0; i < cameras.Length; i++)
+        {
+            if (i == index) continue;
+            cameras[i].SetActive(false);
+        }
+    }
 
     // Update is called once per frame
     void Update()
