ab93a55306b613448a143c3f8869c09a17864625
Doggitoz <cpwheeler4@gmail.com>
2023-03-05 22:57:43 -0600

    Final minus remote issues

--- School Escape/Assets/Scripts/Remote.cs
+++ School Escape/Assets/Scripts/Remote.cs
@@ -5,15 +5,28 @@ using UnityEngine;
 public class Remote : MonoBehaviour
 {
     [SerializeField] Trigger remoteTrigger;
+    [SerializeField] GameObject[] batteries;
+    [SerializeField] GameObject remoteLight;
+    [SerializeField] Material onLight;
+    [SerializeField] Chalkboard chalkboard;
     int batCount = 0;
     public void AddBattery()
     {
         batCount++;
+        for (int i = 0; i < batCount; i++)
+        {
+            if (i > batteries.Length) continue;
+            batteries[i].SetActive(true);
+        }
         if (batCount >= 2)
         {
             remoteTrigger.Unlock();
         }
     }
 
-    
+    public void TurnOn()
+    {
+        remoteLight.GetComponent<MeshRenderer>().material = onLight;
+        chalkboard.RevealLetters();
+    }
 }
